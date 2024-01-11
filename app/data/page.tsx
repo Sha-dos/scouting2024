e5c64ded8c09bdfa12e4f92e1e85688daaf35eda
Sha-dos <77746688+Sha-dos@users.noreply.github.com>
2024-01-11 13:10:39 -0600

    basic app

---
+++ app/data/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function DataPage() {
+    return (
+        <div>
+            <h1 className={title()}>Data</h1>
+        </div>
+    );
+}
