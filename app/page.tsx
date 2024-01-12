179ac1070d795368ec81ff55688c42dd7d7db68c
Sha-dos <77746688+Sha-dos@users.noreply.github.com>
2024-01-12 12:55:46 -0600

    basic firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+'use client';
+
 import {title} from "@/components/primitives";
+import {Button} from "@nextui-org/button";
+import {getDatabase, set, ref} from "@firebase/database";
 
 export default function Home() {
+
+	function upload() {
+		const db = getDatabase();
+
+		set(ref(db, 'matches/0'), {
+			teams: [2502, 2502, 2502, 2502, 2502, 2502]
+		});
+	}
+
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div>
 				<h1 className={title()}>Home</h1>
 			</div>
+
+			<Button variant="bordered" color="primary" onPress={() => {upload()}}>
+				Upload Data
+			</Button>
 		</section>
 	);
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,7 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import {initializeApp} from "@firebase/app";
 
 export interface ProvidersProps {
 	children: React.ReactNode;
@@ -13,6 +14,19 @@ export interface ProvidersProps {
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyDuDKBerihVko_8_c9uFvnAkhKYMyDPPV0",
+		authDomain: "scouting2024-bd9f1.firebaseapp.com",
+		databaseURL: "https://scouting2024-bd9f1-default-rtdb.firebaseio.com",
+		projectId: "scouting2024-bd9f1",
+		storageBucket: "scouting2024-bd9f1.appspot.com",
+		messagingSenderId: "1072635708570",
+		appId: "1:1072635708570:web:1a8dc5b2af865268e24b23",
+		measurementId: "G-XN5MS3V5MJ"
+	};
+
+	const app = initializeApp(firebaseConfig);
 
 	return (
 		<NextUIProvider navigate={router.push}>
