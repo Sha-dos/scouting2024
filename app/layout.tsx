498968efb6e8686a02a990131a8d806ad906e13c
Nolan Peterson <ndpete2007@gmail.com>
2024-02-21 21:17:30 -0600

    PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,53 @@ import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 import {Dropdown, DropdownItem, DropdownMenu, DropdownTrigger} from "@nextui-org/react";
 import {Button} from "@nextui-org/button";
+import type { Metadata, Viewport } from "next";
 
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
+export const metadata: Metadata = {
+	applicationName: APP_NAME,
+	title: {
+		default: APP_DEFAULT_TITLE,
+		template: APP_TITLE_TEMPLATE,
+	},
+	description: APP_DESCRIPTION,
+	manifest: "/manifest.json",
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: "default",
+		title: APP_DEFAULT_TITLE,
+		// startUpImage: [],
+	},
+	formatDetection: {
+		telephone: false,
+	},
+	openGraph: {
+		type: "website",
+		siteName: APP_NAME,
+		title: {
+			default: APP_DEFAULT_TITLE,
+			template: APP_TITLE_TEMPLATE,
+		},
+		description: APP_DESCRIPTION,
+	},
+	twitter: {
+		card: "summary",
+		title: {
+			default: APP_DEFAULT_TITLE,
+			template: APP_TITLE_TEMPLATE,
+		},
+		description: APP_DESCRIPTION,
+	},
+};
+
+export const viewport: Viewport = {
+	themeColor: "#FFFFFF",
+};
+/*
 export const metadata: Metadata = {
 	title: {
 		default: siteConfig.name,
@@ -19,7 +65,7 @@ export const metadata: Metadata = {
 		{ media: "(prefers-color-scheme: light)", color: "white" },
 		{ media: "(prefers-color-scheme: dark)", color: "black" },
 	],
-};
+};*/
 
 export default function RootLayout({
 	children,
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export"
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+    nextConfig
+});
+
+//module.exports = nextConfig
