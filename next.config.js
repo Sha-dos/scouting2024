a0453756039f5976f016381adbc530ae8afe15ae
Nolan Peterson <ndpete2007@gmail.com>
2024-02-21 21:46:46 -0600

    write static to /out

--- next.config.js
+++ next.config.js
@@ -2,13 +2,8 @@ const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export"
-}
-
 module.exports = withPWA({
-    nextConfig
+    output: "export"
 });
 
 //module.exports = nextConfig
