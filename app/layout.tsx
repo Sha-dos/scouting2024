eaf586b15b958750a04d384d154362fd0be69481
Nolan Peterson <ndpete2007@gmail.com>
2024-03-08 20:04:26 -0600

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({
 			>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
-            <Navbar />
+						<Navbar />
 						<main className="container mx-auto max-w-7xl flex-grow">
 							{/*children*/}
 							<a>currently down</a>
